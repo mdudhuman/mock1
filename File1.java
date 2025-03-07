74616eaea1dc9a14deb83a4b1900d6ead466af41
mdudhuman <mdudhuman@gmail.com>
2025-03-07 19:57:19 +0000

    2. Adding User2 two java files

---
+++ File1.java
@@ -0,0 +1,1 @@
+public class File1 { public static void main(String[] args) { System.out.println('Hello from File1'); } }
---
+++ File2.java
@@ -0,0 +1,1 @@
+public class File2 { public static void main(String[] args) { System.out.println('Hello from File2'); } }
